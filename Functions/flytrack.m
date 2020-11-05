fc5be466ee0d84855da207fa159451084df662ba
jonahpearl <j4jonahp@gmail.com>
2020-11-05 18:52:13 -0500

    Use imbinarize instead of std thresholding.

--- Functions/flytrack.m
+++ Functions/flytrack.m
@@ -125,28 +125,264 @@ n_created = 0;
 clear props; % This step seems needed no matter what
 
 % Prime the props structure to record the properties of flies
-props(1:nfly,1:nframe) = struct('Centroid',zeros(1,2,'double')); %,'Area',[],'MajorAxisLength',[],'MinorAxisLength',[],'Eccentricity',[],'Orientation',[]);
+props(1:nfly,1:nframe) = struct('Centroid',zeros(1,2,'double'), 'Area', [], 'Eccentricity', []); %,'Area',[],'MajorAxisLength',[],'MinorAxisLength',[],'Eccentricity',[],'Orientation',[]);
 
 % If quiet mode is off, let people know what is going on
 if quietmode == 0
     dispbar = waitbar(0,['Tracking Video',num2str(vid_num),'- Arena',num2str(arena_num)]);
 end
 
+% Params
+min_fly_size = 15; % 10 for ln138, 15 for test2
+max_ecc = 0.99;
+matingFly_minArea = 130; % 80 for ln138, 130 for test2
+gamma = 2;
+master_gamma = gamma;
+
+% Store data
+areasByFrame_cell = cell(1, nframe);
+eccByFrame_cell = cell(1,nframe);
+nFliesByFrame = zeros(1, nframe);
+matingByFrame = zeros(1, nframe);
+
+% debugging
+singleRegion_gamma2_maxAreas = [];
+
 for i=1:nframe
 %     tic
     % Subtract the background and threshold the frame
-    arena_rev_nbg_bw = flytrackbw( Arena, i, background, gamma, custom_bw_threshold_modifier);
     
-    % Remove the moon-shaped ring before erosion
-    % JP notes: this sometimes creates an issue when fly is touching the
-    % edge of the arena, so one or both flies are removed along with the
-    % "moon" (the edge of the arena).
-    [ arena_rev_nbg_bw, Flags(i) ] = flytrackdemoon( arena_rev_nbg_bw, demoon_cutoff);
+    % ln138_test
+    %{
+    JP: in heatrig, sometimes edge of arena is fairly bright, and is
+    actually brighter than the flies, causing you to lose a fly. This is
+    usually solved by MATLAB's imbinarize, but that often works too well
+    -- mating pairs then appear as one big clump, instead of two. That's
+    maybe ok in another program, but flyknight expects each fly to show up
+    separately (hence SZ's really stringent thresholding, so that even
+    mating flies will be separated into different regions).
     
-    % Erode the images (get rid of small shades) and label them
-    arena_rev_nbg_bw_erode(:,:,i) = imerode(arena_rev_nbg_bw,strel('disk', flysize));
-    [ arena_rev_nbg_bw_erode_lb(:,:,i) , nflydetected(i) ] = bwlabel( arena_rev_nbg_bw_erode(: , : , i ));
+    Hmm, what if we actually changed that? What if we use imbinarize,
+    demoon, and just count the number of clumps of an appropriate size?
+    It's stupid simple but it could work...
     
+    Ground truth: mating frame 14-112
+    
+    Case studies
+    
+    Frame 3: one fly is good. The other fly is too dark and shows up as a
+    tiny dot. There is a large dot on the edge of the arena. So fly size
+    sanity check won't catch that, and reduction will end up discarding the
+    real fly. This is solved by imbinarize.
+    -- Smaller of two flies here is 29, larger is 68.
+    
+    Frame 52: there is a one-pixel spot left over after erosion that is
+    detected as a fly in line 164. Manually fixing and going to watershedding fixes it.
+    Solution: instead of just checking nflies, do a sanity check on the
+    size of the flies. If there are some really tiny spots, eliminate them?
+    --with imbinarize, noise is all <10, mating pair is 99.
+    
+    Frame 45: one-pixel spot along with one small spot for the flies. Would
+    end up taking previous frame. Sanity check on fly size would solve
+    this.
+    --Now with imbinarize, the noise is too large and would appear as a
+    second fly (sz of 30) even though they're mating (pair has sz of 77).
+    
+    
+    
+    Frame 68: flies are too dark. Even a gamma of 2 loses them. Gamma of 1
+    finds them. It detects some noise as three very tiny "flies" instead,
+    then uses reduction to reduce to two. Again, sanity check on fly size
+    would solve this up front. Basically check the size of the initially
+    detected flies (in line 176), and see, oh, they're unrealistically
+    small. We must have lost track of them. Increase gamma and try again.
+    -- with imbinarize and gamma of 1, mating pair is 139, noise is 1 and
+    4. Good!
+    
+    Frame 65: detects one big blob (mating pair) and some noise. Fly size
+    limit would solve this.
+    
+    Frames 9: imposing eccentricity limit (no more than 0.98?) will solve
+    this.
+    
+    Frame 10: one fly is detected with gamma of 2, but two flies are
+    detected with gamma of 1. Using gamma of 1 the whole time solves.
+    
+    Frame 132: issue is that the fly is connected to the perimeter of the
+    well. Intermediate gamma of 1.5 solves this.
+    
+    Frame 134: Just really dark. Gamma of 0.75 finds both flies. Gamma of 1
+    finds the first fly, then the algorithm moves on.
+    
+    ***
+    
+    -- Try with gamma = 2. 
+        -- If you find two regions of the right area and the
+        right eccentricity, etc, it's two flies and you're done.
+        -- If you find one region, either they're mating, or the other fly
+        is too dark to see. So decrement gamma and see what happens.
+    -- Try with gamma decreased.
+        -- If you find two regions now, there are two flies.
+        -- Otherwise, they're mating.
+    
+    --With this set up, a final area of 80 or larger seems to be a
+    reliable indicator of mating.
+    
+    Frame 2: fly is removed as part of the edge. Institute a min mating
+    size?
+    Frame 13: fly is removed as part of the edge.
+    Frame 15: mating pair is seen as two separate regions due to a
+    not-quite-low-enough gamma. Why isn't erosion followed by dilation?
+        -- Because then you have too much noise, it seems.
+        -- Maybe try a conditional dilation or something but it gets messy.
+    
+    
+    Frame 6: too many "flies". Could be solved by taking two largest, which
+    are under 80 at a gamma of 1. Or just check if any is large enough to
+    be a mating pair.
+    
+    Frame 20: too many "flies." The mating pair isn't large enough to catch
+    that way. The noise is too prominent to fully remove, so you end up
+    with three "flies" when there's really just one mating pair.
+        -- Increasing gamma solves this, and makes the mating pair large
+        enough to be noticed as such.
+    
+    Frame 32: too many "flies". Mating pair is large enough with a gamma of
+    2 that a size catch would catch this.
+    
+
+    ***
+    Frame 141: classic issue. With gamma = 2, one fly is lost, and it sees
+    one fly that isn't big enough to be a mating pair. With gamma = 1.5, it
+    sees both flies, but now one is big enough to be a mating pair even
+    though it's only one fly (bc gamma decr). So maybe need to have a
+    moving threshold for mating pair size depending on gamma? Doesn't feel
+    quite right though...
+    
+    %}
+    
+    % test2
+    %{
+    Ok, we definitely overfit to the last video's data lol.
+    
+    Ground truth: 16-81.
+    
+    -- Simply increasing the size of the mating fly min area helps a lot. But
+    there are a good number of false positives.
+    -- These are improved drastically by removing the program's ability to
+    say that if there are 2+ flies, it might really be noise + mating.
+        -- Ok, so I think this feature ^ was just a weird necessity due to
+        weird lighting conditions of the first video. Hopefully if we
+        create uniform lighting conditions, we won't need that catch.
+    
+    %}
+    regionCriteriaFlag = 0;
+    tries = 0;
+    
+    while ~regionCriteriaFlag && tries < 3
+        arena_rev_nbg_bw = flytrackbw( Arena, i, background, gamma, custom_bw_threshold_modifier);
+
+        % Remove the moon-shaped ring before erosion
+        % JP notes: this sometimes creates an issue when fly is touching the
+        % edge of the arena, so one or both flies are removed along with the
+        % "moon" (the edge of the arena).
+        [ arena_rev_nbg_bw, Flags(i) ] = flytrackdemoon( arena_rev_nbg_bw, demoon_cutoff);
+
+        % Erode the images (get rid of small shades) and label them
+        arena_rev_nbg_bw_erode(:,:,i) = imerode(arena_rev_nbg_bw,strel('disk', flysize));
+%         arena_rev_nbg_bw_erode(:,:,i) = imopen(arena_rev_nbg_bw, strel('disk', flysize));
+        [ arena_rev_nbg_bw_erode_lb(:,:,i) , nflydetected(i) ] = bwlabel( arena_rev_nbg_bw_erode(: , : , i ));
+        
+        thisFrameProps = regionprops(arena_rev_nbg_bw_erode_lb(:,:,i),'Area', 'Eccentricity');
+        
+        flySize_bool = [thisFrameProps.Area] > min_fly_size;
+        flyEcc_bool = [thisFrameProps.Eccentricity] < max_ecc;
+        
+        nfly_JP = sum(flySize_bool & flyEcc_bool);
+        if  nfly_JP == 0 
+            % no flies detected
+            gamma = gamma - 0.5;
+            tries = tries + 1;
+        elseif nfly_JP == 1
+            % one fly detected; either mating or other is too dark.
+            
+            % debugging:
+            singleRegion_gamma2_maxAreas = [singleRegion_gamma2_maxAreas thisFrameProps(flySize_bool & flyEcc_bool).Area];
+            
+            % implement choice based on the region size: mating pairs are
+            % large.
+            if thisFrameProps(flySize_bool & flyEcc_bool).Area > matingFly_minArea
+                % mating pair
+                regionCriteriaFlag = 1;
+                matingByFrame(i) = 1;
+            else
+                % one fly too dark
+                gamma = gamma - 0.5;
+                tries = tries + 1;
+            end
+            
+        elseif nfly_JP == 2
+            
+%             % Check if one of them is large enough to be a mating pair
+%             if max([thisFrameProps(flySize_bool & flyEcc_bool).Area]) > matingFly_minArea
+%                 % mating pair
+%                 nfly_JP = 1;
+%                 matingByFrame(i) = 1;
+%             end
+            
+            regionCriteriaFlag = 1;
+        else
+            % oops, too many flies!
+            
+%             % Check if one of them is large enough to be a mating pair
+%             if max([thisFrameProps(flySize_bool & flyEcc_bool).Area]) > matingFly_minArea
+%                 % mating pair
+%                 nfly_JP = 1;
+%                 matingByFrame(i) = 1;
+%                 regionCriteriaFlag = 1;
+%             else
+%                 % Sometimes you get mating pairs that are split because
+%                 % it's too dark. 
+%                 gamma = gamma - 0.5;
+%                 tries = tries + 1;
+%             end
+            
+            gamma = gamma - 0.5;
+            tries = tries + 1;
+        end
+    end
+    
+    
+    % After this progression, if still only one "fly", it's a mating pair
+    nFliesByFrame(i) = nfly_JP;
+    if nfly_JP == 0
+        fprintf('Frame %d, no flies found, gamma %d \n', i, gamma)
+        areasByFrame_cell{i} = NaN;
+        eccByFrame_cell{i} = NaN;
+    elseif nfly_JP == 1 && matingByFrame(i) == 0
+        fprintf('Frame %d, one non-mating fly found, gamma %d \n', i, gamma)
+        areasByFrame_cell{i} = thisFrameProps(flySize_bool & flyEcc_bool).Area;
+        eccByFrame_cell{i} = thisFrameProps(flySize_bool & flyEcc_bool).Eccentricity;
+    elseif nfly_JP == 1 && matingByFrame(i) == 1
+        fprintf('Frame %d, one mating pair found, gamma %d \n', i, gamma)
+        areasByFrame_cell{i} = [thisFrameProps(flySize_bool & flyEcc_bool).Area];
+        eccByFrame_cell{i} = [thisFrameProps(flySize_bool & flyEcc_bool).Eccentricity];
+    elseif nfly_JP == 2
+        fprintf('Frame %d, two flies found, gamma %d \n', i, gamma)
+        areasByFrame_cell{i} = [thisFrameProps(flySize_bool & flyEcc_bool).Area];
+        eccByFrame_cell{i} = [thisFrameProps(flySize_bool & flyEcc_bool).Eccentricity];
+    else
+        warning('More than two flies detected for frame %d', i)
+    end
+    
+    % Reset gamma
+    gamma = master_gamma;
+    
+    
+    % If num of flies detected is too low, it's going to be because a)
+    % nothing is there (in which case take previous frame); or b) because only
+    % one spot is there after image processing. If it's a big spot, we can
+    % use some tricks to separate it into two flies.
     if nflydetected(i) < nfly % Need watershed
         %disp(['Frame ' , num2str(i) , ' Watershedding'])
         n_watershed = n_watershed + 1;
@@ -190,10 +426,11 @@ for i=1:nframe
         % never fails
         Flags(i) =Flags(i) + 1;
         nflydetected(i) = nfly;
-        arena_rev_nbg_bw_erode_lb(:,:,i) = flytrackreduction( arena_rev_nbg_bw_erode_lb(:,:,i) , nfly );
+        arena_rev_nbg_bw_erode_lb(:,:,i) = flytrackreduction(arena_rev_nbg_bw_erode_lb(:,:,i) , nfly );
     end
     
-    props(:,i)=regionprops(arena_rev_nbg_bw_erode_lb(:,:,i),'Centroid'); %,'Orientation','Area','Eccentricity','MajorAxisLength','MinorAxisLength');
+    props(:,i) = regionprops(arena_rev_nbg_bw_erode_lb(:,:,i),'Centroid', 'Area', 'Eccentricity'); %,'Orientation','Area','Eccentricity','MajorAxisLength','MinorAxisLength');
+    
     
     if quietmode==0
         waitbar(i/nframe,dispbar)
